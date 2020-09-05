06a6d8335366218a93a1864243dba503b1c8d0ba
GeorgeNLQ <70286427+GeorgeTorres-py@users.noreply.github.com>
2020-09-05 15:27:02 -0700

    Update prayerbot.py

--- prayerbot.py
+++ prayerbot.py
@@ -1,13 +1,11 @@
-def Prayer():
+lambda Prayer:
 	
-	question = input('Would you like me to say a prayer for you?: ')
+question = input('Would you like me to say a prayer for you?: ')
 	
 if question.strip().lower() == 'y':
 	name = input('What is your name?: ')
 	print('Lord God, please watch over %s and protect %s. Bless %s with favor, prosperity, and the unconditional love of Jesus Christ. In Jesus\n Amen' % (name,name))
 else:
 	print("Praise Jesus!") 
-return
-
 
 Prayer()
